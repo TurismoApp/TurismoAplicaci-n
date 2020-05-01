5755b8fc91f3618e8f8a0d824bf4a312aca96579
andr11s <alvisandres1@gmail.com>
2020-04-30 19:56:59 -0500

    ADD font y detailsActivity

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from "@angular/core";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { Routes } from "@angular/router";
-
-import { ItemsComponent } from "./item/items.component";
-import { ItemDetailComponent } from "./item/item-detail.component";
+import { AppComponent } from "./app.component";
+import { DetailsActivityComponent } from './details-activity/details-activity.component'
 
 const routes: Routes = [
-    { path: "", redirectTo: "/items", pathMatch: "full" },
-    { path: "items", component: ItemsComponent },
-    { path: "item/:id", component: ItemDetailComponent }
+    { path: "", component: DetailsActivityComponent }
+
 ];
 
 @NgModule({
