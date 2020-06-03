89fb1df0753f03ce0d91aa9be1df6e353268a303
Royber <roybermanjarrez@gmail.com>
2020-06-03 18:53:39 -0500

    ajustado el servicio

--- src/service/place.service.ts
+++ src/service/place.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { place } from '~/models/place.model';
+import { host } from '~/database/host.database';
 
+const getRute = '/places/';
 @Injectable({
     providedIn: 'root'
 })
@@ -10,7 +12,7 @@ export class placeService {
     constructor(private httpClient: HttpClient) { }
 
     getPlace(id: number) {
-        return this.httpClient.get<place[]>('https://311f1684181c.ngrok.io' + '/places/' + id)
+        return this.httpClient.get<place[]>(host + getRute + id);
     }
 
 }
